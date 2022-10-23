9bc94fbd1c094ae9ecdef457ea96c3e69d6713f6
George Kakarlis <georgekakarlis@yahoo.com>
2022-10-23 12:57:02 +0200

    first commit

---
+++ api/v1/router/router.go
@@ -0,0 +1,29 @@
+package router
+
+import (
+	"example/go-backend/handler"
+	"example/go-backend/middleware"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+// SetupRoutes setup router api
+func SetupRoutes(app *fiber.App) {
+	// Middleware
+	api := app.Group("/api", logger.New())
+	api.Get("/", handler.Hello)
+
+	// Auth
+	auth := api.Group("/auth")
+	auth.Post("/login", handler.Login)
+
+	// User
+	user := api.Group("/user")
+	user.Get("/:id", handler.GetUser)
+	user.Post("/", handler.CreateUser)
+	user.Patch("/:id", middleware.Protected(), handler.UpdateUser)
+	user.Delete("/:id", middleware.Protected(), handler.DeleteUser)
+
+	
+}
